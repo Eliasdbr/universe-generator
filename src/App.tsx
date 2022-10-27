ab4611012a4ae4f0ece85c65e46282701bf8d7a6
eliasdbr <eliasdbr@outlook.com>
2022-10-27 00:57:06 -0300

    Added NavBar Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
+import NavBar from './components/NavBar';
 import Home from './views/Home';
 import PlanetSystem from './views/PlanetSystem';
 
@@ -11,7 +12,8 @@ function App() {
 			<Routes>
 				<Route
 					path='/*'
-					element={
+					element={<div style={style}>
+						<NavBar/>
 						<Routes>
 							<Route
 								path=''
@@ -26,11 +28,18 @@ function App() {
 								element={<Navigate to='/'/>}
 							/>
 						</Routes>
-					}
+					</div>}
 				/>
 			</Routes>
 		</BrowserRouter>
 	);
 }
+
+const style: CSSProperties = {
+	display: 'flex',
+	flexDirection: 'row',
+	width: '100vw',
+	maxHeight: '100vh',
+};
 
 export default App;
---
+++ src/components/NavBar/index.tsx
@@ -0,0 +1,19 @@
+/**
+ * Navigation side/top bar
+ */
+
+import React, { CSSProperties } from "react";
+
+const NavBar = () => {
+	return <div style={style}>
+
+	</div>;
+}
+
+const style: CSSProperties = {
+	height: '100%',
+	width: '64px',
+	backgroundColor: 'gray',
+}
+
+export default NavBar;
